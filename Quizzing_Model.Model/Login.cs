05c61969c9b1de7d420e6cfe6d3a0d42fdc5fe4a
Aarsh Prajapati <124061739+AarshPrajapati@users.noreply.github.com>
2023-02-10 14:40:34 +0530

    Add files via upload

---
+++ Quizzing_Model.Model/Login.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Quizzing_Model.Model
+{
+    public  class Login
+    {
+        public int id { get; set; }
+        [Required]
+        [DisplayName("First Name :-")]
+        [RegularExpression(@"(a-zA-Z)", ErrorMessage = "Enter Only Character")]
+        public string F_name { get; set; }
+        [Required]
+        [DisplayName("Last Name :- ")]
+        public string L_name { get; set; }
+        [Required]
+        [DisplayName("Username :- ")]
+        public string username { get; set; }
+        [Required]
+        [EmailAddress]
+        [DisplayName("E-Mail:- ")]
+        public string E_mail { get; set; }
+        [Required]
+        [MaxLength(10)]
+        [DisplayName("Password :- ")]
+        public string password { get; set; }
+
+    }
+}
